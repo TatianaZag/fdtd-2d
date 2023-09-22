a6ea5a39fc0b90cd314eb0edc246ec45de590144
Tatiana <NikolFilomena@yandex.ru>
2023-09-23 01:01:00 +0300

    Script upgrade

--- OpenMP/run.sh
+++ OpenMP/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#`
+N=10
 for dat in -DSMALL_DATASET -DMEDIUM_DATASET -DLARGE_DATASET
 do
     for num in 1 2 4 8 12 20
@@ -9,9 +9,12 @@ do
     echo "-------< Dataset: $dat count tasks: $num >-------"
     echo
     make clean;
-        for ((i = 0; i < 10; i++))
+        sum=0
+        for ((i = 0; i < $N; i++))
         do #for i
-        CFLAGS="$dat -DNUM_TASKS=$num" make
+        res=$(CFLAGS="$dat -DNUM_TASKS=$num" make | grep -Eo "[0-9]+\.[0-9]+")
+        sum=$(echo $res + $sum | bc)
         done #for i
+        echo "scale=6; $sum/$N" | bc
     done #for num
 done #for dat
